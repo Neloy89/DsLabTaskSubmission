b4a4bbab722581cad5e48f2c3ac15f29d5336431
Neloy89 <117824493+Neloy89@users.noreply.github.com>
2023-12-07 22:51:34 +0600

    Binarytree(final_4).cpp

--- Binarytree(final_4).cpp
+++ Binarytree(final_4).cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+// Structure for a binary tree node
 struct Node
 {
   int data;
@@ -8,33 +9,37 @@ struct Node
   Node* right;
 };
 
+// Function to perform an inorder traversal of the binary tree
 void inorder(Node* root)
 {
   if (root == NULL)
     return;
-  inorder(root->left);
-  cout << root->data << " -> ";
-  inorder(root->right);
+  inorder(root->left);  // Visit left subtree
+  cout << root->data << " -> ";  // Print the current node
+  inorder(root->right); // Visit right subtree
 }
 
+// Function to perform a preorder traversal of the binary tree
 void preorder(Node* root)
 {
   if (root == NULL)
     return;
-  cout << root->data << " -> ";
-  preorder(root->left);
-  preorder(root->right);
+  cout << root->data << " -> ";  // Print the current node
+  preorder(root->left); // Visit left subtree
+  preorder(root->right); // Visit right subtree
 }
 
+// Function to perform a postorder traversal of the binary tree
 void postorder(Node* root)
 {
   if (root == NULL)
     return;
-  postorder(root->left);
-  postorder(root->right);
-  cout << root->data << " -> ";
+  postorder(root->left); // Visit left subtree
+  postorder(root->right); // Visit right subtree
+  cout << root->data << " -> ";  // Print the current node
 }
 
+// Function to create a new node with given data
 Node* createNode(int item)
 {
   Node* newNode = new Node;
@@ -44,12 +49,14 @@ Node* createNode(int item)
   return newNode;
 }
 
+// Function to insert a new node as the left child of a given node
 Node* insertLeft(Node* root, int item)
 {
   root->left = createNode(item);
   return root->left;
 }
 
+// Function to insert a new node as the right child of a given node
 Node* insertRight(Node* root, int item)
 {
   root->right = createNode(item);
@@ -58,19 +65,22 @@ Node* insertRight(Node* root, int item)
 
 int main()
 {
+  // Creating a binary tree
   Node* root = createNode(7);
   insertLeft(root, 12);
   insertRight(root, 29);
   insertLeft(root->left, 15);
   insertRight(root->left, 26);
 
-  cout << "Inorder  \n";
+  // Displaying the traversals
+  cout << "Inorder traversal\n";
   inorder(root);
-  cout << "\nPreorder \n";
+  cout << "\nPreorder traversal\n";
   preorder(root);
-  cout << "\nPostorder  \n";
+  cout << "\nPostorder traversal\n";
   postorder(root);
 
+  // Deallocating memory (freeing nodes)
   delete root->left;
   delete root->right;
   delete root;
